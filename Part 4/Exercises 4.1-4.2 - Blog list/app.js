1d62ee00215a597d5a2550cdfb8605a07fe8df44
Vinícius Bastos <viniciusbsilva57@gmail.com>
2024-01-05 02:57:10 -0300

    Exercises 4.1-4.2 done

---
+++ Part 4/Exercises 4.1-4.2 - Blog list/app.js
@@ -0,0 +1,31 @@
+const blogsRouter = require("./controller/blogs");
+const middleware = require("./utils/middleware");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+
+mongoose.set("strictQuery", false);
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info("Connected to MongoDB");
+    })
+    .catch((error) => {
+        logger.error("Error connecting to MongoDB", error.message);
+    });
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.use("/api/blogs", blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ Part 4/Exercises 4.1-4.2 - Blog list/utils/middleware.js
@@ -0,0 +1,24 @@
+const logger = require("./logger");
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+    logger.error(error.message);
+
+    if (error.name === "CastError") {
+        response.status(400).send({ error: "malformatted id" });
+        return;
+    } else if (error.name === "ValidationError") {
+        response.status(400).json({ error: error.message });
+        return;
+    }
+
+    next(error);
+};
+
+module.exports = {
+    unknownEndpoint,
+    errorHandler,
+};
