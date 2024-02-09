b60576bbfefd25e5367691dc97cdc27ae3b2d15b
Vinícius Bastos <viniciusbsilva57@gmail.com>
2024-02-09 13:39:29 -0300

    Exercises 4.20 done

--- Part 4/Exercises 4.1-4.25 - Blog list/controller/blogs.js
+++ Part 4/Exercises 4.1-4.25 - Blog list/controller/blogs.js
@@ -37,7 +37,19 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-    await Blog.findByIdAndDelete(request.params.id);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: "Token invalid" });
+    }
+
+    const blog = await Blog.findById(request.params.id);
+
+    if (blog.user.toString() !== decodedToken.id) {
+        return response
+            .status(401)
+            .json({ error: "You are not authorized to edit this blog." });
+    }
+    await Blog.deleteOne(blog);
 
     response.status(204).end();
 });
